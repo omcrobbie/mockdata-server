cdb3281c2cc62ec0b8114f8702e04cada5033cfe
omcrobbie <olivermcrobbie@northwesternmutual.com>
2019-03-03 16:47:05 -0500

    customize server logging

--- src/serve.js
+++ src/serve.js
@@ -2,16 +2,24 @@ const jsonServer = require('json-server');
 const { outputPath } = require('./paths');
 const server = jsonServer.create();
 const router = jsonServer.router(outputPath);
-const middlewares = jsonServer.defaults();
-const { green } = require('chalk');
-
+const middlewares = jsonServer.defaults({ logger: false });
+const { green, yellow, red } = require('chalk');
+const morgan = require('morgan');
+setupMorgan();
 server
 	.use(middlewares)
+	.use(morgan(`${yellow('Used mock')}: :method :url :color-status :response-time`))
 	.use(jsonServer.bodyParser)
-// .use(jsonServer.rewriter({
-//     '/oneShow/:id' : 'oneShow'
-// }))
 	.use('/api', router)
 	.listen(3001, () => {
 		console.log(green('Mock-data server is listening on port 3001...'));
 	});
+
+function setupMorgan () {
+	morgan.token('color-status', (req, res) => {
+		if (res.statusCode >= 400) {
+			return red(res.statusCode);
+		}
+		return green(res.statusCode);
+	});
+}
