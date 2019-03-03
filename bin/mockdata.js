ccbc82000eedb0e313e668f764be8b58bf711209
omcrobbie <olivermcrobbie@northwesternmutual.com>
2019-03-03 14:51:29 -0500

    basic package working / cleanup

--- bin/mockdata.js
+++ bin/mockdata.js
@@ -2,9 +2,14 @@
 'use strict';
 const spawn = require('cross-spawn');
 const script = process.argv[2];
+const serverStart = process.argv[3];
 
+if (serverStart) {
+	const [command, ...args] = serverStart.split(' ');
+	spawn(command, args, { stdio: 'inherit' });
+}
 if (script === 'consume' || script === 'serve') {
-	spawn.sync('node', [require.resolve(`../src/${script}`)], { stdio: 'inherit' });
+	spawn('node', [require.resolve(`../src/${script}`)], { stdio: 'inherit' });
 } else {
 	console.log('Script wrong or undefined: must be "consume" or "serve"');
 	process.exit(0);
--- src/serve.js
+++ src/serve.js
@@ -1,18 +1,17 @@
-const path = require('path');
 const jsonServer = require('json-server');
+const { outputPath } = require('./paths');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '../apps', `${process.argv[2]}.json`));
+const router = jsonServer.router(outputPath);
 const middlewares = jsonServer.defaults();
+const { green } = require('chalk');
 
-module.exports = () => {
-	server
-		.use(middlewares)
-		.use(jsonServer.bodyParser)
-	// .use(jsonServer.rewriter({
-	//     '/oneShow/:id' : 'oneShow'
-	// }))
-		.use('/api', router)
-		.listen(3001, () => {
-			console.log('Mock server is listening on port 3001...');
-		});
-}
+server
+	.use(middlewares)
+	.use(jsonServer.bodyParser)
+// .use(jsonServer.rewriter({
+//     '/oneShow/:id' : 'oneShow'
+// }))
+	.use('/api', router)
+	.listen(3001, () => {
+		console.log(green('Mock-data server is listening on port 3001...'));
+	});
